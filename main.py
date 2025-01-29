b5f2048afbd7cd69c3cea70a6aabf03667754b85
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-29 12:05:55 +0700

    Data Query Done

--- main.py
+++ main.py
@@ -27,19 +27,29 @@ con = sqlite3.connect(f"{db_folder}/demodb.db")
 #                         FOREIGN KEY (director_id) REFERENCES directors(id)
 #                     );''')
 
-con.execute("INSERT INTO genres (name) VALUES ('Action')")
-con.execute("INSERT INTO genres (name) VALUES ('Drama')")
-con.execute("INSERT INTO genres (name) VALUES ('Comedy')")
-con.execute("INSERT INTO genres (name) VALUES ('Sci-Fi')")
+# con.execute("INSERT INTO genres (name) VALUES ('Action')")
+# con.execute("INSERT INTO genres (name) VALUES ('Drama')")
+# con.execute("INSERT INTO genres (name) VALUES ('Comedy')")
+# con.execute("INSERT INTO genres (name) VALUES ('Sci-Fi')")
     
-con.execute("INSERT INTO directors (name) VALUES ('Steven Spielberg')")
-con.execute("INSERT INTO directors (name) VALUES ('Christopher Nolan')")
-con.execute("INSERT INTO directors (name) VALUES ('Martin Scorsese')")
+# con.execute("INSERT INTO directors (name) VALUES ('Steven Spielberg')")
+# con.execute("INSERT INTO directors (name) VALUES ('Christopher Nolan')")
+# con.execute("INSERT INTO directors (name) VALUES ('Martin Scorsese')")
     
-con.execute("INSERT INTO movies (title, release_year, genre_id,director_id) VALUES ('Jurassic Park', 1993, 1, 1)")
-con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('Inception', 2010, 4, 2)")
-con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('The Wolf of Wall Street', 2013, 2, 3)")
-con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('The Dark Knight', 2008, 1, 2)")
-con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('E.T.', 1982, 1, 1)")
+# con.execute("INSERT INTO movies (title, release_year, genre_id,director_id) VALUES ('Jurassic Park', 1993, 1, 1)")
+# con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('Inception', 2010, 4, 2)")
+# con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('The Wolf of Wall Street', 2013, 2, 3)")
+# con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('The Dark Knight', 2008, 1, 2)")
+# con.execute("INSERT INTO movies (title, release_year, genre_id, director_id) VALUES ('E.T.', 1982, 1, 1)")
 
-con.commit()
+# con.commit()
+
+#select all movies directed by Christopher Nolan
+print("Movies Directed by Christopher Nolen")
+cursor = con.execute("SELECT m.title, m.release_year, g.name FROM movies m "
+                        "JOIN genres g ON m.genre_id = g.id "
+                        "JOIN directors d ON m.director_id = d.id "
+                        "WHERE d.name = 'Christopher Nolan'")
+
+for row in cursor:
+    print(row)
